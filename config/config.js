16298468f597d32a3a2467c95a7b7e0b3e732fb2
janneth Paez <janneth.paez03@gmail.com>
2016-07-12 09:54:07 -0500

    lab006 config Se cargan las configuraciones de un json

--- config/config.js
+++ config/config.js
@@ -1,30 +1,30 @@
 // leyendo archivo de configuraciones
 var fs = require("fs");
- 
- module.exports = function () {
 
-   fs.readFile("./config.json","utf-8",
-     function (err,content){
-    if(err){
-        console.log("> ERROR NO SE PUDO LEER" +
-            "archivo de configuracion: config.json...");
-        console.log("> Se carga configuracion" +
-       "por defecto..." );
-       return {
-           IP: '127.0.0.1',
-           PORT: 3030
+module.exports = function () {
+
+  fs.readFile("./config.json","utf-8",
+ function(err,content){
+     if(err){
+         console.log(">ERROR: NO SE PUDO LEER" +
+           "archivo de configuracion: config.json..." );
+           console.log("> Se carga configuracion"+ 
+             "por defecto...");
+             return {
+                 IP: '127.0.0.1',
+                 PORT: 3030
+             }
+         //
+       }else{
+           //
+           //
+           var configObj = JSON.parse(content);
+           configObj.IP = process.env.IP || configObj.IP;
+           configObj.PORT = process.env.PORT || config.Obj.PORT;
+           //
+           return configObj;
        }
-        //tomo alguna polotica
-    }else{
-        // Convirtiendo el json string
-        // en un json objeto de json
-        var configObj = JSON.parse(content);
-        configObj.IP = process.env.IP || configObj.IP;
-        config.PORT = process.env.PORT || CONFIGoBJ.port;
-        // Exporto objeto de configuracion
-        return configObj;
-    }
+ });
+}
 
-  });
 
- }
--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 // http
 var http = require("http");
-// crear un servidor basico
-var server = http.createServer(function(req,res){
+var fs= require('fs');
+var config = require("./config/config.js");
+//
+//
+var PORT = config.env.PORT;
+var IP = config.env.IP;
+if (IP=='127.0.0.1'){
+    console.log(">----EJECUTANDO EN MODO LOCAL");
+}
+
+//
+var server = http.createServer(function(req, res){
     //
     //
-    res.whiteHead(200,{
+    res.writeHead(200,{
         "Content-Type" : "text/plain",
         "Server" : "ITGAM@4.2.4"
     });
-    //
-    res.white("hola mundo");
-    //
-    res.end();
+    //lectura
+    fs.readFile('./static/index.html',
+    'utf8',function(err, content){
+        if(err){
+        res.write("<h1>ERROR DE LECTURA</h1>");
+
+        }else{
+             res.end(content);
+        }
+    });
+   
 });
 //poner a trabajar al server
-server.listen(3000, '127.0.0.1',function(){
-    console.log(">Server listering @http://localhost:3000...");
+server.listen(PORT,IP,function(){
+    console.log(`>Server listering @http://${IP}:${PORT}...`);
 
 });
